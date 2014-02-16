ee7a5e0fd0ce17219abfd1f3ed451af03a90a795
Jef Claes <jef.claes@gmail.com>
2014-02-16 21:31:37 +0100

    Passport

--- src/web/app.js
+++ src/web/app.js
@@ -1,20 +1,52 @@
-var util = require('util');
-var express = require('express');
-var expressValidator = require('express-validator');
-var async = require('async');
-var pg = require('pg'); 
-var er = require('./eventsourcing.js');
-var pj = require('./projections.js');
-var ev = require('./events.js');
-var config = require('./config.js');
+var util = require('util')
+	, express = require('express')
+	, expressValidator = require('express-validator')
+	, async = require('async')
+	, pg = require('pg')
+	, er = require('./eventsourcing.js')
+	, pj = require('./projections.js')
+	, ev = require('./events.js')
+	, config = require('./config.js')
+	, passport = require('passport')
+ 	, TwitterStrategy = require('passport-twitter').Strategy;
 
 var app = express();
+
+passport.serializeUser(function(user, done) {	
+  	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {	
+	done(null, obj);
+});
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.consumerKey,
+    consumerSecret: config.twitter.consumerSecret,
+    callbackURL: config.twitter.callbackURL
+  },
+  function(token, tokenSecret, profile, done) {
+  	process.nextTick(function () { 
+    	return done(null, profile);
+    });
+  }
+));
 
 app.configure (function(){
     app.use(express.logger({ format: ":method :url" }));        
     app.use(express.bodyParser());        
     app.use(expressValidator()); 
+    app.use(express.cookieParser());
+    app.use(express.session({ secret : 'changeit' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 });
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/index');
+  });
 
 app.get('/index', function(req, res) {	
 	res.sendfile('index.html');		
@@ -137,6 +169,14 @@ app.post('/commands/consume', function(req, res) {
 	});
 	
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { 
+  	return next(); 
+  }
+  
+  res.redirect('/login')
+}
 
 module.exports = app;
 
--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,10 @@
 var config = { 
-	connectionstring : "pg://postgres:admin@localhost:5432/test"
+	connectionstring : 'pg://postgres:admin@localhost:5432/test',
+	twitter : {
+		consumerKey: 'nyouNGuN5MQnh6YOx7Uw',
+    	consumerSecret: 'vPMY1DrnE4x3PYLYobxWZawHsv1iQwmxi6qpg6woUio',
+    	callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+	}
 };
 
 module.exports = config;
