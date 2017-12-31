a1ca8e54d5351c52d867a1de0efbcbc8aa6f25a8
Jef Claes <jef.claes@gmail.com>
2017-12-31 15:46:52 +0100

    Update config.js

--- infrastructure/config.js
+++ infrastructure/config.js
@@ -1,8 +1,8 @@
 var config = { 
 	connectionstring : process.env.DATABASE_URL || 'pg://postgres:admin@localhost:5432/test',
 	twitter : {
-		consumerKey: 'nyouNGuN5MQnh6YOx7Uw',
-    	consumerSecret: 'vPMY1DrnE4x3PYLYobxWZawHsv1iQwmxi6qpg6woUio',
+		consumerKey: 'deleted',
+    	consumerSecret: 'deleted',
     	callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
 	}
 };
